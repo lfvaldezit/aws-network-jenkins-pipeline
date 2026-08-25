6979381504ef4cb6e08974d5e9ecf8d372a7a68f
unknown <lfvaldez.it@gmail.com>
2026-08-25 09:29:06 -0300

    Update terraform version.tf

--- terraform/version.tf
+++ terraform/version.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket = "aws-network-terraform-state-jenkins-2026"
+    key = "network-lab/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    use_lockfile = true
+  }
   required_providers {
     aws = {
       source = "hashicorp/aws"
